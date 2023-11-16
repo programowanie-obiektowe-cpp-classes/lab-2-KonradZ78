4cb6faae476fb0facd9b03d8583e5e9d7f9b6646
KonradZ78 <148917868+KonradZ78@users.noreply.github.com>
2023-11-17 00:27:28 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -3,42 +3,13 @@
 #include "Resource.hpp"
 #include <iostream>
 
-class ResourceManager {
-private:
-    Resource resource; // Właściciel obiektu typu Resource
-
+class ResourceManager
+{
 public:
-    ResourceManager() : resource() {
-        // Konstruktor domyślny
-    }
+    ResourceManager() : resource() {}
 
-    
-    ResourceManager(const ResourceManager& other) : resource(other.resource) {
-    }
+    double get() { return resource.get(); }
 
-    
-    ResourceManager& operator=(const ResourceManager& other) {
-        if (this != &other) {
-            resource = other.resource;
-        }
-        return *this;
-    }
-
-    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource)) {
-    }
-
-    ResourceManager& operator=(ResourceManager&& other) noexcept {
-        if (this != &other) {
-            resource = std::move(other.resource);
-        }
-        return *this;
-    }
-
-    ~ResourceManager() {
-        // Implementacja destruktora
-    }
-
-    double get() {
-        return resource.get(); // Zwraca wynik wywołania metody get obiektu, którym zarządza
-    }
+private:
+    Resource resource;
 };
